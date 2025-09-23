5ea8c81899cd5d6faac193d5014cc5a638ff294d
Nan Zhang <nanzha@meta.com>
2025-09-22 19:53:56 -0700

    Change python grid calc for MTIA back to python mode
    Differential Revision: D83000165

--- torch/_inductor/codegen/wrapper_fxir.py
+++ torch/_inductor/codegen/wrapper_fxir.py
@@ -905,10 +905,7 @@ class FxConverter:
         call_args = self._lookup_args(line.call_args)
         kernel = self.kernels[line.kernel_name]
         tuner = kernel.tuner
-        # Use python_slow mode instead of python mode to avoid
-        # the round to neginf behaviour, which is not the convention
-        # in other languages.
-        tuner.grid_mode = "python_slow"
+        tuner.grid_mode = "python"
 
         # Optionally autotune the kernels.
         # The FX backend currently only supports compile-time tuning.
